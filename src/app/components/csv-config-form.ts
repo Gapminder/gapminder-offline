4ecf23c0f98c5740184e27a7eadc34747b3daa02
buchslava <vyacheslav.chub@gmail.com>
2016-12-27 12:33:07 +0200

    fix

--- src/app/components/csv-config-form.ts
+++ src/app/components/csv-config-form.ts
@@ -4,32 +4,54 @@ import {Component, OnInit, Output, Injectable, EventEmitter} from '@angular/core
 @Component({
   selector: 'ae-csv-config-form',
   template: `
-<div class="popup-block" style="width: 100%; height: 300px; overflow: auto;">
-    <div style="margin-bottom: 20px;">
+<style>
+  .main-popup {
+    width: 100%;
+    height: 300px;
+    overflow: auto;
+  }
+  .step-block {
+    margin-bottom: 20px;
+  }
+  .step-label {
+    font-size: large;
+  }
+  .example-image {
+    max-width: 100%;
+    height: auto;
+    padding-right: 10px;
+  }
+  .ext-radio-btn {
+    background-color: #f4f4f4;
+    border-color: #a5a5a5;
+  }
+</style>
+<div class="popup-block" class="main-popup">
+    <div class="step-block">
         <h4>Step 1: Choose how your data is arranged:</h4>
         <div>
-            <label [class.selected]="choiceRows.checked" style="font-size: large"><input type="radio" name="choice" #choiceRows (change)="setChoice('rows')"> Time is in rows</label>
+            <label [class.selected]="choiceRows.checked" class="step-label"><input type="radio" name="choice" #choiceRows (change)="setChoice('rows')"> Time is in rows</label>
             <div style="font-size: small">Column 1: entities, Column 2: time points, Column 3 and on: indicators (<a href="#" (click)="switchExampleRows()">see example</a>)</div>
         </div>
-        <div *ngIf="isExampleRows" style="width: 100%;"><img src="./public/images/templates/time-as-rows-example.png" style="max-width: 100%; height: auto; padding-right: 10px;"></div>
+        <div *ngIf="isExampleRows" style="width: 100%;"><img src="./public/images/templates/time-as-rows-example.png" class="example-image"></div>
         <div>
-            <label [class.selected]="choiceColumns.checked" style="font-size: large;"><input type="radio" name="choice" #choiceColumns (change)="setChoice('columns')"> Time is in columns</label>
+            <label [class.selected]="choiceColumns.checked" class="step-label"><input type="radio" name="choice" #choiceColumns (change)="setChoice('columns')"> Time is in columns</label>
             <div style="font-size: small">Column 1: entities, Column 2: indicators, Column 3 and on: time points (<a href="#" (click)="switchExampleColumns()">see example</a>)</div>
         </div>
-        <div *ngIf="isExampleColumns" style="width: 100%;"><img src="./public/images/templates/time-as-columns-example.png" style="max-width: 100%; height: auto; padding-right: 10px;"></div>
+        <div *ngIf="isExampleColumns" style="width: 100%;"><img src="./public/images/templates/time-as-columns-example.png" class="example-image"></div>
     </div>
     
-    <div style="margin-bottom: 20px;">
+    <div class="step-block">
         <h4 *ngIf="choice">Step 2: Pick a file:</h4>
         <div *ngIf="choice"><input type="file" (change)="onCsvFileChanged($event)" /></div>
     </div>
     
-    <div style="margin-bottom: 20px;">
+    <div class="step-block">
         <h4 *ngIf="file">Step 3: Additional options:</h4>
         <div *ngIf="file" class="btn-group">Delimiter:</div>
         <div *ngIf="file" class="btn-group">
-            <label class="btn btn-default" style="background-color: #f4f4f4; border-color: #a5a5a5;" [(ngModel)]="delimiter" btnRadio="," uncheckable> , </label>
-            <label class="btn btn-default" style="background-color: #f4f4f4; border-color: #a5a5a5;" [(ngModel)]="delimiter" btnRadio=";" uncheckable> ; </label>
+            <label class="btn btn-default ext-radio-btn" [(ngModel)]="delimiter" btnRadio="," uncheckable> , </label>
+            <label class="btn btn-default ext-radio-btn" [(ngModel)]="delimiter" btnRadio=";" uncheckable> ; </label>
         </div>
     </div>
 </div>
