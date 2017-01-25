709a9784fc8a1c7a770c45cd048c23f22aef9522
Vyacheslav Chub <vyacheslav.chub@valor-software.com>
2017-01-25 19:38:08 +0200

    fix

--- update-dataset-win32.bat
+++ update-dataset-win32.bat
@@ -1,5 +1,7 @@
-ren cache\ddf--gapminder--systema_globalis-* cache\ddf--gapminder--systema_globalis
-robocopy "cache\ddf--gapminder--systema_globalis-*" ".\resources\app\ddf--gapminder--systema_globalis" /E
+cd cache
+mv ddf--gapminder--systema_globalis* ddf--gapminder--systema_globalis
+robocopy ddf--gapminder--systema_globalis ..\resources\app\ddf--gapminder--systema_globalis
+cd ..
 rmdir /s /q cache
 del .\update-required
 start "" ".\Gapminder Offline.exe"
