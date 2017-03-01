87ec1c683e62d451be8c77245189936d97dfb577
Vyacheslav Chub <vyacheslav.chub@valor-software.com>
2017-03-01 12:38:36 +0200

    dmg

--- package-dmg.js
+++ package-dmg.js
@@ -1,28 +1,14 @@
 const appdmg = require('appdmg');
-const ee = appdmg({source: 'dmg-builder/dmg-config.json', target: 'dmg-builder/test.dmg'});
+const ee = appdmg({source: 'dmg-builder/dmg-config.json', target: 'release/Install Gapminder Offline.dmg'});
 
 ee.on('progress', info => {
-
-  // info.current is the current step
-  // info.total is the total number of steps
-  // info.type is on of 'step-begin', 'step-end'
-
-  console.log(info.current, info.total, info.type);
-
-  // 'step-begin'
-  // info.title is the title of the current step
-
-  // 'step-end'
-  // info.status is one of 'ok', 'skip', 'fail'
-
+  console.log('Build DMG: step ', info.current, ' of ', info.total);
 });
 
 ee.on('finish', () => {
-  // There now is a `test.dmg` file
-  console.log('DMG was created');
+  console.log('DMG was created...');
 });
 
 ee.on('error', (err) => {
   console.log('DMG creation error: ', err);
-  // An error occurred
 });
