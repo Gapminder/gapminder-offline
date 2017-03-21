f41fae79ca8c0c200866df43ca232e58917f7814
Vyacheslav Chub <vyacheslav.chub@valor-software.com>
2017-03-21 10:33:56 +0200

    chore(general): fix export template

--- copy-css-win.bat
+++
@@ -1,6 +0,0 @@
-copy .\node_modules\vizabi\build\dist\vizabi.css .\src\app
-rem copy .\node_modules\vizabi-barrankchart\build\dist\barrankchart.css .\src\app
-copy .\node_modules\vizabi-bubblechart\build\dist\bubblechart.css .\src\app
-copy .\node_modules\vizabi-bubblemap\build\dist\bubblemap.css .\src\app
-copy .\node_modules\vizabi-mountainchart\build\dist\mountainchart.css .\src\app
-rem copy .\node_modules\vizabi-linechart\build\dist\linechart.css .\src\app
---
+++ vizabi-copy-win.bat
@@ -0,0 +1,39 @@
+copy .\node_modules\vizabi-ddfcsv-reader\dist\vizabi-ddfcsv-reader.js .\src\app\export-template\libs
+
+copy .\node_modules\vizabi\build\dist\vizabi.js .\src\app\libs
+copy .\node_modules\vizabi\build\dist\vizabi.js .\src\app\export-template\libs
+copy .\node_modules\vizabi\build\dist\vizabi.css .\src\app\libs
+copy .\node_modules\vizabi\build\dist\vizabi.css .\src\app\export-template\libs
+
+copy .\node_modules\vizabi\build\dist\assets\cursors\* .\src\app\assets\cursors
+copy .\node_modules\vizabi\build\dist\assets\cursors\* .\src\app\export-template\assets\cursors
+
+copy .\node_modules\vizabi\build\dist\assets\translation\* .\src\app\preview-data\translation
+
+copy .\node_modules\vizabi\build\dist\assets\translation\* .\src\app\assets\translation
+copy .\node_modules\vizabi\build\dist\assets\translation\* .\src\app\export-template\assets\translation
+
+rem copy .\node_modules\vizabi-barrankchart\build\dist\barrankchart.css .\src\app\libs
+rem copy .\node_modules\vizabi-barrankchart\build\dist\barrankchart.css .\src\app\export-template\libs
+rem copy .\node_modules\vizabi-barrankchart\build\dist\barrankchart.js .\src\app\libs
+rem copy .\node_modules\vizabi-barrankchart\build\dist\barrankchart.js .\src\app\export-template\libs
+
+copy .\node_modules\vizabi-bubblechart\build\dist\bubblechart.css .\src\app\libs
+copy .\node_modules\vizabi-bubblechart\build\dist\bubblechart.css .\src\app\export-template\libs
+copy .\node_modules\vizabi-bubblechart\build\dist\bubblechart.js .\src\app\libs
+copy .\node_modules\vizabi-bubblechart\build\dist\bubblechart.js .\src\app\export-template\libs
+
+copy .\node_modules\vizabi-bubblemap\build\dist\bubblemap.css .\src\app\libs
+copy .\node_modules\vizabi-bubblemap\build\dist\bubblemap.css .\src\app\export-template\libs
+copy .\node_modules\vizabi-bubblemap\build\dist\bubblemap.js .\src\app\libs
+copy .\node_modules\vizabi-bubblemap\build\dist\bubblemap.js .\src\app\export-template\libs
+
+copy .\node_modules\vizabi-mountainchart\build\dist\mountainchart.css .\src\app\libs
+copy .\node_modules\vizabi-mountainchart\build\dist\mountainchart.css .\src\app\export-template\libs
+copy .\node_modules\vizabi-mountainchart\build\dist\mountainchart.js .\src\app\libs
+copy .\node_modules\vizabi-mountainchart\build\dist\mountainchart.js .\src\app\export-template\libs
+
+rem copy .\node_modules\vizabi-linechart\build\dist\linechart.css .\src\app\libs
+rem copy .\node_modules\vizabi-linechart\build\dist\linechart.css .\src\app\export-template\libs
+rem copy .\node_modules\vizabi-linechart\build\dist\linechart.js .\src\app\libs
+rem copy .\node_modules\vizabi-linechart\build\dist\linechart.js .\src\app\export-template\libs
