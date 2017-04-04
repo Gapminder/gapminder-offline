2e8fc5561601c4e0401c5a36ce73aad9ad685c3b
Vyacheslav Chub <vyacheslav.chub@valor-software.com>
2017-04-04 15:20:54 +0300

    fix move command in update dataset

--- update-app-src/update-dataset.cpp
+++ update-app-src/update-dataset.cpp
@@ -4,7 +4,7 @@
 
 int main() {
   Sleep(3000);
-  system("cd cache & mv ddf--gapminder--systema_globalis* ddf--gapminder--systema_globalis & robocopy ddf--gapminder--systema_globalis ..\\resources\\app\\ddf--gapminder--systema_globalis /E & cd .. & rmdir /s /q cache & del .\\update-required & start \"\" \".\\Gapminder Offline.exe\"");
+  system("cd cache & powershell.exe -nologo -noprofile -command \"& {Get-ChildItem -Filter ddf--gapminder--systema_globalis* | Rename-Item -NewName ddf--gapminder--systema_globalis}\" & robocopy ddf--gapminder--systema_globalis ..\\resources\\app\\ddf--gapminder--systema_globalis /E & cd .. & rmdir /s /q cache & del .\\update-required & start \"\" \".\\Gapminder Offline.exe\"");
 
   return 0;
 }
