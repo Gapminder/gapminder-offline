2773bf481d1cc45343922e3fb329901692e6fd99
Vyacheslav Chub <vyacheslav.chub@valor-software.com>
2017-04-04 12:26:51 +0300

    fix

--- update-app-src/update-app.cpp
+++ update-app-src/update-app.cpp
@@ -4,7 +4,7 @@
 
 int main() {
   Sleep(3000);
-  system("cd cache & robocopy . .. /XF update-app-win64.exe update-dataset-*.exe /E & cd .. & rmdir /s /q cache & del .\\release.zip & del .\\update-required & start \"\" \".\\Gapminder Offline.exe\"");
+  system("cd cache & robocopy . .. /XF update-app-win64.exe update-dataset-win64.exe update-app-win32.exe update-dataset-win32.exe /E & cd .. & rmdir /s /q cache & del .\\release.zip & del .\\update-required & start \"\" \".\\Gapminder Offline.exe\"");
 
   return 0;
 }
