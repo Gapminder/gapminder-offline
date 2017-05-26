4e1fb179032bdd97b2137720618b01dc0bdd338c
Vyacheslav Chub <vyacheslav.chub@valor-software.com>
2017-05-26 17:06:15 +0300

    change generic chart config

--- src/app/components/tabs/chart.service.ts
+++ src/app/components/tabs/chart.service.ts
@@ -70,106 +70,11 @@ export class ChartService {
     tab.extResources = tabDataDescriptor.extResources;
     tab.additionalData = this.ddfFolderDescriptor.additionalData;
 
-    // if () {
     const chartType = tab.chartType;
     const config = !this.ddfFolderDescriptor.ddfUrl || this.ddfFolderDescriptor.ddfUrl.includes('systema_globalis') ? configSg[chartType] : {
-
-
-      "state": {
-      "entities": {
-        "dim": "geo",
-          "show": {
-          "is--country": true
-        }
-      },
-      "entities_colorlegend": {
-        "dim": "world_4region"
-      },
-      "entities_tags": {
-        "dim": "tag"
-      },
-      "time": {
-        "startOrigin": "1800",
-          "endOrigin": "2015",
-          "value": "2015",
-          "dim": "time"
-      },
-      "marker": {
-        "space": ["entities", "time"],
-          "label": {
-          "use": "property",
-            "which": "name"
-        },
-        "axis_y": {
-          "use": "indicator",
-            "which": "life_expectancy_years",
-            "zoomedMin": 19,
-            "zoomedMax": 86,
-            "domainMin": 0,
-            "domainMax": 100
-        },
-        "axis_x": {
-          "use": "indicator",
-            "scaleType": "log",
-            "domainMax": 150000,
-            "domainMin": 300,
-            "zoomedMax": 150000,
-            "zoomedMin": 300,
-            "which": "income_per_person_gdppercapita_ppp_inflation_adjusted"
-        },
-        "size": {
-          "use": "indicator",
-            "which": "population_total",
-            "domainMin": 15,
-            "domainMax": 1400000000,
-            "scaleType": "linear",
-            "allow": {
-            "scales": ["linear"]
-          }
-        },
-        "color": {
-          "use": "property",
-            "which": "world_4region",
-            "scaleType": "ordinal",
-            "syncModels": ["marker_colorlegend"]
-        }
-      },
-      "marker_colorlegend":{
-        "space": ["entities_colorlegend"],
-          "opacityRegular": 0.8,
-          "opacityHighlightDim": 0.3,
-          "label": {
-          "use": "property",
-            "which": "name"
-        },
-        "hook_rank": {
-          "use": "property",
-            "which": "rank"
-        },
-        "hook_geoshape": {
-          "use": "property",
-            "which": "shape_lores_svg"
-        }
-      },
-      "marker_tags": {
-        "space": ["entities_tags"],
-          "label": {
-          "use": "property",
-            "which": "name"
-        },
-        "hook_parent": {
-          "use": "property",
-            "which": "parent"
-        }
+      ui: {
+        splash: true
       }
-    },
-      "ui": {
-      "datawarning": {
-        "doubtDomain": [1800, 1950, 2015],
-          "doubtRange": [1.0, 0.3, 0.2]
-      },
-      "splash": true
-    }
     };
 
     config.data = {
@@ -194,7 +99,6 @@ export class ChartService {
     if (onChartReady) {
       onChartReady(tab);
     }
-    // }
   }
 
   public newSimpleChart(tabsModel: TabModel[], properties: any, onChartReady?: Function): void {
