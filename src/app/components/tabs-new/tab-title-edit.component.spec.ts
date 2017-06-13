5e8d1e4cc0d07d9f55a5393a28fe4797b609e9e6
Andrey Konovka <legionivo@gmail.com>
2017-06-13 11:05:20 +0300

    (test) Fix formatting, fix TSLint errors. .editorconfig added

--- src/app/components/tabs-new/tab-title-edit.component.spec.ts
+++ src/app/components/tabs-new/tab-title-edit.component.spec.ts
@@ -1,62 +1,62 @@
-import { TestBed, ComponentFixture } from "@angular/core/testing";
-import {TabTitleEditComponent} from "./tab-title-edit.component";
-import {FormsModule} from "@angular/forms";
+import {TestBed, ComponentFixture} from '@angular/core/testing';
+import {TabTitleEditComponent} from './tab-title-edit.component';
+import {FormsModule} from '@angular/forms';
 
 describe('Component: TabTitleEditComponent', () => {
-    let component: TabTitleEditComponent;
-    let fixture: ComponentFixture<TabTitleEditComponent>;
-    let context;
+  let component: TabTitleEditComponent;
+  let fixture: ComponentFixture<TabTitleEditComponent>;
+  let context;
 
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            imports: [FormsModule],
-            declarations: [TabTitleEditComponent],
-        });
-        fixture = TestBed.createComponent(TabTitleEditComponent);
-
-        component = fixture.componentInstance;  // to access properties and methods
-        context = fixture.debugElement.componentInstance;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [FormsModule],
+      declarations: [TabTitleEditComponent]
     });
+    fixture = TestBed.createComponent(TabTitleEditComponent);
 
-    it('should check that the component work', () => {
-        expect(context).toBeTruthy();
-    });
+    component = fixture.componentInstance;  // to access properties and methods
+    context = fixture.debugElement.componentInstance;
+  });
 
-    it('should check doBlur()', () => {
-        context.doBlur();
-        expect(context.blur).toBeDefined();
-    });
+  it('should check that the component work', () => {
+    expect(context).toBeTruthy();
+  });
 
-    it('should check ngAfterContentInit()', () => {
-        const showSpy = spyOn(context.tabEditInput.nativeElement, 'focus').and.stub();
-        context.ngAfterContentInit();
-        expect(showSpy).toHaveBeenCalled();
-    });
+  it('should check doBlur()', () => {
+    context.doBlur();
+    expect(context.blur).toBeDefined();
+  });
 
-    it('should check doEnter()', () => {
-        const showSpy = spyOn(context.enter, 'emit').and.stub();
-        context.doEnter();
-        expect(context.enter).toBeDefined();
+  it('should check ngAfterContentInit()', () => {
+    const showSpy = spyOn(context.tabEditInput.nativeElement, 'focus').and.stub();
+    context.ngAfterContentInit();
+    expect(showSpy).toHaveBeenCalled();
+  });
 
-        context.title = 'title';
-        context.doEnter();
-        expect(showSpy).toHaveBeenCalled();
-    });
+  it('should check doEnter()', () => {
+    const showSpy = spyOn(context.enter, 'emit').and.stub();
+    context.doEnter();
+    expect(context.enter).toBeDefined();
 
-    it('should check doEsc()', () => {
-        const showSpy = spyOn(context.esc, 'emit').and.stub();
-        context.doEsc();
-        expect(context.esc).toBeDefined();
+    context.title = 'title';
+    context.doEnter();
+    expect(showSpy).toHaveBeenCalled();
+  });
 
-        context.title = 'title';
-        context.doEsc();
-        expect(showSpy).toHaveBeenCalled();
-    });
+  it('should check doEsc()', () => {
+    const showSpy = spyOn(context.esc, 'emit').and.stub();
+    context.doEsc();
+    expect(context.esc).toBeDefined();
 
-    it('should check update()', () => {
-        const title = 'test';
-        context.update(title);
-        expect(context.title).toBe('test');
-    });
+    context.title = 'title';
+    context.doEsc();
+    expect(showSpy).toHaveBeenCalled();
+  });
+
+  it('should check update()', () => {
+    const title = 'test';
+    context.update(title);
+    expect(context.title).toBe('test');
+  });
 
 });
--- src/app/components/tabs-new/tab.component.spec.ts
+++ src/app/components/tabs-new/tab.component.spec.ts
@@ -1,53 +1,52 @@
-import {TestBed, ComponentFixture} from "@angular/core/testing";
-import {TabNewComponent} from "./tab.component";
-import {MessageService} from "../../message.service";
+import {TestBed, ComponentFixture} from '@angular/core/testing';
+import {TabNewComponent} from './tab.component';
+import {MessageService} from '../../message.service';
 
 describe('Component: TabNewComponent', () => {
-    let component: TabNewComponent;
-    let fixture: ComponentFixture<TabNewComponent>;
-    let context;
+  let component: TabNewComponent;
+  let fixture: ComponentFixture<TabNewComponent>;
+  let context;
 
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            declarations: [TabNewComponent],
-            providers: [MessageService]
-        });
-        fixture = TestBed.createComponent(TabNewComponent);
-
-        component = fixture.componentInstance;  // to access properties and methods
-        context = fixture.debugElement.componentInstance;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [TabNewComponent],
+      providers: [MessageService]
     });
+    fixture = TestBed.createComponent(TabNewComponent);
 
-    it('should check that the component work', () => {
-        expect(context).toBeTruthy();
-    });
+    component = fixture.componentInstance;  // to access properties and methods
+    context = fixture.debugElement.componentInstance;
+  });
 
-    it('should check that getDisplayStyle() returns block value', () => {
-        context.active = true;
-        expect(context.getDisplayStyle()).toBe("block");
-    });
+  it('should check that the component work', () => {
+    expect(context).toBeTruthy();
+  });
 
-    it('should check that getDisplayStyle() returns none value', () => {
-        context.active = false;
-        expect(context.getDisplayStyle()).toBe("none");
-    });
+  it('should check that getDisplayStyle() returns block value', () => {
+    context.active = true;
+    expect(context.getDisplayStyle()).toBe('block');
+  });
 
-    it('should check applyEditedTitle()', () => {
-        spyOn(context, 'applyEditedTitle');
-        context.applyEditedTitle();
-        expect(context.applyEditedTitle).toHaveBeenCalled();
-    });
+  it('should check that getDisplayStyle() returns none value', () => {
+    context.active = false;
+    expect(context.getDisplayStyle()).toBe('none');
+  });
 
-    it('should check that applyEditedTitle()', () => {
-        context.applyEditedTitle();
-        expect(context.editMode).toBeFalsy();
-    });
+  it('should check applyEditedTitle()', () => {
+    spyOn(context, 'applyEditedTitle');
+    context.applyEditedTitle();
+    expect(context.applyEditedTitle).toHaveBeenCalled();
+  });
 
-    it('should check that dismissEditedTitle()', () => {
-        context.dismissEditedTitle();
-        expect(context.editMode).toBeFalsy();
-        expect(component.title).toEqual(context.titleCopy);
-    });
+  it('should check that applyEditedTitle()', () => {
+    context.applyEditedTitle();
+    expect(context.editMode).toBeFalsy();
+  });
+
+  it('should check that dismissEditedTitle()', () => {
+    context.dismissEditedTitle();
+    expect(context.editMode).toBeFalsy();
+    expect(component.title).toEqual(context.titleCopy);
+  });
 
 });
-
