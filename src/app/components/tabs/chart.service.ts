f9bc7789f71caab8d6279310d842f3d1d458138f
Angie the Fairytale <konzeptmeister@gmail.com>
2017-09-15 18:48:51 +0200

    Fix(chart.service): add file last modified date before reading it #51

--- src/app/components/tabs/chart.service.ts
+++ src/app/components/tabs/chart.service.ts
@@ -3,6 +3,8 @@ import { TabModel } from './tab.model';
 import { DdfFolderDescriptor } from '../descriptors/ddf-folder.descriptor';
 import { TabDataDescriptor } from '../descriptors/tab-data.descriptor';
 
+const fs = require('fs');
+
 // const PopByAge = ;
 const BarRankChart = require('vizabi-barrankchart');
 const BubbleChart = require('vizabi-bubblechart');
@@ -96,29 +98,34 @@ export class ChartService {
   public newSimpleChart(tabsModel: TabModel[], properties: any, onChartReady?: Function): void {
     const newTab = new TabModel(properties.chartType, true);
 
-    newTab.chartType = properties.chartType;
-    newTab.model = {
-      data: {
-        reader: properties.reader,
-        delimiter: properties.delimiter,
-        path: properties.path
+    fs.stat(properties.path, (err: any, stats: any) => {
+
+      newTab.chartType = properties.chartType;
+      newTab.model = {
+        data: {
+          reader: properties.reader,
+          delimiter: properties.delimiter,
+          path: properties.path,
+          lastModified: stats.mtime.valueOf()
+        }
+      };
+
+      if (properties.state) {
+        newTab.model.state = properties.state;
       }
-    };
 
-    if (properties.state) {
-      newTab.model.state = properties.state;
-    }
+      if (this.isDevMode) {
+        this.log(JSON.stringify(newTab.model));
+      }
 
-    if (this.isDevMode) {
-      this.log(JSON.stringify(newTab.model));
-    }
+      tabsModel.forEach((tab: TabModel) => tab.active = false);
+      tabsModel.push(newTab);
 
-    tabsModel.forEach((tab: TabModel) => tab.active = false);
-    tabsModel.push(newTab);
+      if (onChartReady) {
+        onChartReady();
+      }
 
-    if (onChartReady) {
-      onChartReady();
-    }
+    });
   }
 
   public getCurrentTab(tabsModel: TabModel[]): TabModel {
