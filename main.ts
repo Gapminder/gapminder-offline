ac642d79a61deb48f38d6a07bb8e0858dceb9418
Dmitry Basov <dmbasov@gmail.com>
2021-07-14 02:04:49 +0300

    enable electron remote

--- main.ts
+++ main.ts
@@ -67,7 +67,8 @@ function createWindow() {
 
   mainWindow = new BrowserWindow({
     width: 1200, height: 800, webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      enableRemoteModule: true
     }
   });
   mainWindow.appPath = nonAsarAppPath;
