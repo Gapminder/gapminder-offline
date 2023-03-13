66cdf6f2158c2f45c5a2059bbf9d375c617b0343
Dmitry Basov <dmbasov@gmail.com>
2023-03-14 01:20:55 +0300

    fix tools translation for serve

--- angular.webpack.js
+++ angular.webpack.js
@@ -1,9 +1,22 @@
 /**
  * Custom angular webpack configuration
  */
+const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 
- module.exports = (config, options) => {
+module.exports = (config, options) => {
+    //check for serve
+    if (config.devServer) {
+        config.plugins.push(new CopyPlugin({
+            patterns: [{
+                context: path.resolve(__dirname, "preview-data", "translation"),
+                from: "**/*",
+                to: "assets/translation/"
+            }]
+        }))
+    }
+
     if (config.optimization.minimizer[0]) {
         config.optimization.minimizer[0] = (
             new TerserPlugin({
